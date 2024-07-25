4d2caf6f1a602cea0899ed9c815ef1e23cd27b85
YordanPKS <140574806+YordanPKS@users.noreply.github.com>
2024-07-24 23:50:11 -0400

    Add files via upload

---
+++ nuevaruta.sh
@@ -0,0 +1,8 @@
+termux-setup-storage
+cd /storage/emulated/0/
+mkdir descargasFlix
+cd /storage/emulated/0/descargasFlix
+echo "import cubaflixmax" > exep.py
+cd 
+cd Termux-Facil
+bash rutap.sh
---
+++ rutap.sh
@@ -0,0 +1,5 @@
+cd
+echo "cd /storage/emulated/0/descargasFlix" > flix.sh
+echo "python3 exep.py" >> flix.sh
+bash flix.sh
+

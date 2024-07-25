6691a944dc9a25c340f8b5d07bbe070b023106fa
YordanPKS <140574806+YordanPKS@users.noreply.github.com>
2024-07-24 23:56:32 -0400

    Delete central.py

--- central.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-print("hola")
-ejecuto = int(input())
-
-if ejecuto == 1 : #instalacion
-    subprocess.run(["pip3", "install", "python3"])
-    subprocess.run(["pip3", "upgrade", "python3"])
